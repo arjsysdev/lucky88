e63fee60a7db6b622ab7611c6fba28d3a96e5940
arjohnsysdev <arjohnsysdev@gmail.com>
2017-04-12 09:55:36 +0800

    new db file

--- lucky88.sql
+++ lucky88.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 06, 2017 at 11:04 AM
+-- Generation Time: Apr 12, 2017 at 09:54 AM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 7.0.9
 
@@ -55,7 +55,8 @@ CREATE TABLE `contact` (
 
 INSERT INTO `contact` (`contact_id`, `comp_code`, `contact_type`, `agent`, `comp_name`, `payable_to`, `address`, `area`, `tin_num`, `cont_pers`, `telephone`, `mob_no`, `other_telephone`, `fax_num`, `email`, `website`, `prepared_by`, `update_by`, `prep_date`, `up_date`) VALUES
 (1, 'ARJOHN', 'Customer', '', 'Arjohn Group of Companies', 'test', 'test', 'Area1', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'admin', 'admin', '0000-00-00', '2016-11-02'),
-(2, 'RRX', 'Both', '', 'RRX Subsidiary of Arjohn Corp. ', 'Arjohn Quijano', 'Quijano St.', 'Area1', '12345678', 'Arjohn QUijano', '123', '123456789', '123', '123456789', 'arjohn.official@gmail.com', 'argofc.com', 'admin', NULL, '2016-11-02', NULL);
+(2, 'RRX', 'Both', '', 'RRX Subsidiary of Arjohn Corp. ', 'Arjohn Quijano', 'Quijano St.', 'Area1', '12345678', 'Arjohn QUijano', '123', '123456789', '123', '123456789', 'arjohn.official@gmail.com', 'argofc.com', 'admin', NULL, '2016-11-02', NULL),
+(3, 'ABC100', 'Supplier', '', 'Abaca 100', 'Arjohn', 'Planet Mars', 'Area1', '7894561230', 'Arjohn', '123456879', '0123456789', '123456789', '123456789', 'arjohn@abaca100.com', 'www.abaca100.com', 'arjohn', NULL, '2017-01-13', NULL);
 
 -- --------------------------------------------------------
 
@@ -113,7 +114,9 @@ INSERT INTO `cplist` (`id`, `contact_id`, `start_date`, `end_date`, `product_id`
 (7, 1, '2017-01-01', '2017-01-31', 'TEST100', 125, 0, 4, 4, 4, 4, 'test', '2017-01-03 06:51:19'),
 (8, 2, '2017-01-01', '2017-01-31', 'TEST100', 130, 0, 4, 4, 4, 4, 'test', '2017-01-03 06:51:24'),
 (9, 2, '2017-01-01', '2017-01-31', 'BGARJOHN', 145, 0, 4, 4, 4, 4, 'test', '2017-01-03 06:49:57'),
-(10, 1, '2017-01-01', '2017-01-31', 'BGARJOHN', 160, 0, 4, 4, 4, 4, 'test', '2017-01-03 06:50:18');
+(10, 1, '2017-01-01', '2017-01-31', 'BGARJOHN', 160, 0, 4, 4, 4, 4, 'test', '2017-01-03 06:50:18'),
+(11, 1, '2017-03-01', '2017-03-31', 'TEST100', 200, 0, 1, 1, 1, 0, 'test', '2017-03-13 02:15:38'),
+(12, 1, '2017-03-01', '2017-03-31', 'BGARJOHN', 525, 2, 2, 2, 2, 2, 'test', '2017-03-15 06:32:03');
 
 -- --------------------------------------------------------
 
@@ -219,6 +222,43 @@ CREATE TABLE `order_items` (
   `date_submitted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`id`, `soid`, `product_code`, `contact_id`, `qty`, `unit`, `price`, `amount`, `less1`, `less2`, `less3`, `date_submitted`) VALUES
+(1, 1, 'TEST100', 1, 2, '2', 200, 1, 1, 1, 1, '2017-03-13 02:16:02'),
+(2, 2, 'BGARJOHN', 1, 5, '2', 525, 2, 2, 2, 2, '2017-03-15 06:32:28'),
+(3, 2, 'TEST100', 1, 5, '2', 200, 1, 1, 1, 1, '2017-03-15 06:32:28');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `porder_items`
+--
+
+CREATE TABLE `porder_items` (
+  `id` int(11) NOT NULL,
+  `soid` int(11) NOT NULL,
+  `product_code` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `contact_id` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `unit` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `price` double NOT NULL,
+  `amount` double NOT NULL,
+  `less1` int(11) NOT NULL,
+  `less2` int(11) NOT NULL,
+  `less3` int(11) NOT NULL,
+  `date_submitted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `porder_items`
+--
+
+INSERT INTO `porder_items` (`id`, `soid`, `product_code`, `contact_id`, `qty`, `unit`, `price`, `amount`, `less1`, `less2`, `less3`, `date_submitted`) VALUES
+(1, 1, 'TEST100', 3, 2, '2', 145, 2, 2, 2, 2, '2017-01-17 06:30:05');
+
 -- --------------------------------------------------------
 
 --
@@ -307,6 +347,42 @@ INSERT INTO `product_unit` (`unit_id`, `unit`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `purchaseorder`
+--
+
+CREATE TABLE `purchaseorder` (
+  `id` int(11) NOT NULL,
+  `contact_id` int(11) NOT NULL,
+  `comp_code` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `cpoNum` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `poNum` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `date_ordered` date NOT NULL,
+  `remarks` mediumtext COLLATE utf8_unicode_ci NOT NULL,
+  `grosstotal` double DEFAULT NULL,
+  `dis1less` double DEFAULT NULL,
+  `dis2less` double DEFAULT NULL,
+  `dis3less` double DEFAULT NULL,
+  `netSales` double DEFAULT NULL,
+  `lessVAT12` double DEFAULT NULL,
+  `amountNetVAT` double DEFAULT NULL,
+  `pwdDis` double DEFAULT NULL,
+  `totalAmountDue` double DEFAULT NULL,
+  `preparedby` int(11) NOT NULL,
+  `lasteditby` int(11) NOT NULL,
+  `date_submitted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_modified` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `purchaseorder`
+--
+
+INSERT INTO `purchaseorder` (`id`, `contact_id`, `comp_code`, `cpoNum`, `poNum`, `date_ordered`, `remarks`, `grosstotal`, `dis1less`, `dis2less`, `dis3less`, `netSales`, `lessVAT12`, `amountNetVAT`, `pwdDis`, `totalAmountDue`, `preparedby`, `lasteditby`, `date_submitted`, `date_modified`) VALUES
+(1, 3, 'ABC100', 'ABC10017-0001', 'ABC10017-0001', '2017-01-18', 'test', 290, 5.8, 5.68, 5.57, 272.95, 29.24, 243.7, 0, 272.95, 2, 2, '2017-01-17 06:30:05', NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `raw_materials`
 --
 
@@ -361,6 +437,91 @@ INSERT INTO `raw_materials_type` (`rmt_id`, `title`, `is_show`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `receipts`
+--
+
+CREATE TABLE `receipts` (
+  `id` int(11) NOT NULL,
+  `ponum` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `dlvnum` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `sinum` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `drnum` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `smallnum` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `notes` text COLLATE utf8_unicode_ci NOT NULL,
+  `date_modified` timestamp NULL DEFAULT NULL,
+  `date_submitted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `receipts`
+--
+
+INSERT INTO `receipts` (`id`, `ponum`, `dlvnum`, `sinum`, `drnum`, `smallnum`, `notes`, `date_modified`, `date_submitted`) VALUES
+(1, 'ARJOHN17-0002', 'test', 'test', 'test', 'test', 'test', NULL, '2017-03-15 08:48:31'),
+(4, 'ARJOHN17-0002', '17-0002', 'test', 'test', 'test', 'test', NULL, '2017-03-17 09:28:53');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `receipt_items`
+--
+
+CREATE TABLE `receipt_items` (
+  `id` int(11) NOT NULL,
+  `cpo` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `receipt_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `loaded` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `date_submitted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `receipt_items`
+--
+
+INSERT INTO `receipt_items` (`id`, `cpo`, `receipt_id`, `item_id`, `loaded`, `date_submitted`) VALUES
+(1, 'ARJOHN17-0002', 1, 2, '2', '2017-03-15 08:48:31'),
+(2, 'ARJOHN17-0002', 1, 3, '1', '2017-03-15 08:48:32'),
+(7, 'ARJOHN17-0002', 4, 2, '3', '2017-03-17 09:28:53'),
+(8, 'ARJOHN17-0002', 4, 3, '4', '2017-03-17 09:28:53');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `receivingwh`
+--
+
+CREATE TABLE `receivingwh` (
+  `id` int(11) NOT NULL,
+  `comp_code` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `refno` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `type` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `date` date NOT NULL,
+  `remarks` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `received_by` int(11) NOT NULL,
+  `prepared_by` int(11) NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  `date_submitted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `receivingwh_items`
+--
+
+CREATE TABLE `receivingwh_items` (
+  `id` int(11) NOT NULL,
+  `refno` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `loaded` varchar(11) COLLATE utf8_unicode_ci NOT NULL,
+  `date_submitted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `salesorder`
 --
 
@@ -386,6 +547,89 @@ CREATE TABLE `salesorder` (
   `date_submitted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `date_modified` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `salesorder`
+--
+
+INSERT INTO `salesorder` (`id`, `contact_id`, `comp_code`, `cpoNum`, `poNum`, `date_ordered`, `remarks`, `grosstotal`, `dis1less`, `dis2less`, `dis3less`, `netSales`, `lessVAT12`, `amountNetVAT`, `pwdDis`, `totalAmountDue`, `preparedby`, `lasteditby`, `date_submitted`, `date_modified`) VALUES
+(1, 1, 'ARJOHN', 'ARJOHN17-0001', 'ARJOHN17-0001', '2017-03-23', 'test', 400, 4, 3.96, 3.92, 388.12, 41.58, 346.54, 0, 388.12, 1, 1, '2017-03-13 02:16:02', NULL),
+(2, 1, 'ARJOHN', 'ARJOHN17-0002', 'ARJOHN17-0002', '2017-03-23', 'test', 1450, 14.5, 14.36, 14.21, 1406.93, 150.74, 1256.19, 0, 1406.93, 1, 1, '2017-03-15 06:32:27', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `spfreights`
+--
+
+CREATE TABLE `spfreights` (
+  `id` int(11) NOT NULL,
+  `contact_id` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `charge` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `remarks` mediumtext COLLATE utf8_unicode_ci NOT NULL,
+  `date_submitted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `spfreights`
+--
+
+INSERT INTO `spfreights` (`id`, `contact_id`, `start_date`, `end_date`, `charge`, `remarks`, `date_submitted`) VALUES
+(1, 3, '2017-01-01', '2017-01-31', '300', 'test', '2017-01-16 05:12:28');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `splist`
+--
+
+CREATE TABLE `splist` (
+  `id` int(11) NOT NULL,
+  `contact_id` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `product_id` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `price` int(20) NOT NULL,
+  `unit` int(11) NOT NULL,
+  `less1` int(11) DEFAULT NULL,
+  `less2` int(11) DEFAULT NULL,
+  `less3` int(11) DEFAULT NULL,
+  `disperbundle` int(11) NOT NULL,
+  `remarks` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `date_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `splist`
+--
+
+INSERT INTO `splist` (`id`, `contact_id`, `start_date`, `end_date`, `product_id`, `price`, `unit`, `less1`, `less2`, `less3`, `disperbundle`, `remarks`, `date_modified`) VALUES
+(1, 3, '2017-01-01', '2017-01-31', 'TEST100', 145, 0, 2, 2, 2, 2, 'test\r\n', '2017-01-16 05:12:28');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `spstat`
+--
+
+CREATE TABLE `spstat` (
+  `id` int(11) NOT NULL,
+  `contact_id` int(11) NOT NULL,
+  `category` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `terms` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `limitation` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `days` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `date_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `spstat`
+--
+
+INSERT INTO `spstat` (`id`, `contact_id`, `category`, `terms`, `limitation`, `days`, `date_modified`) VALUES
+(1, 3, 'Special', 'Consignment', 'No Limit', '', '2017-01-16 05:12:28');
 
 -- --------------------------------------------------------
 
@@ -418,8 +662,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `salt`, `email`, `activation_code`, `forgotten_password_code`, `forgotten_password_time`, `remember_code`, `created_on`, `last_login`, `active`, `first_name`, `last_name`, `company`, `phone`) VALUES
-(1, '127.0.0.1', 'admin', '$2a$07$SeBknntpZror9uyftVopmu61qg0ms8Qv1yV6FG.kQOSM.9QhmTo36', '', 'admin@admin.com', '', NULL, NULL, NULL, 1268889823, 1482283729, 1, 'Ar', 'John', 'ADMIN', '0'),
-(2, '127.0.0.1', 'arjohn', '$2y$08$gS283kojHwGb7TIsf0rAIOM7WBOuX2yX5v3x.l7scAw.ZYaJ0SzwG', NULL, 'arjohn@admin.com', NULL, 'BhhPDqLczShZsuoTGgQv6.cc7a2757ba5a9fe53f', 1475979873, NULL, 1475978926, 1483578346, 1, 'Arjohn', 'Quijano', 'lucky88', '1234567890'),
+(1, '127.0.0.1', 'admin', '$2a$07$SeBknntpZror9uyftVopmu61qg0ms8Qv1yV6FG.kQOSM.9QhmTo36', '', 'admin@admin.com', '', NULL, NULL, NULL, 1268889823, 1491791707, 1, 'Ar', 'John', 'ADMIN', '0'),
+(2, '127.0.0.1', 'arjohn', '$2y$08$gS283kojHwGb7TIsf0rAIOM7WBOuX2yX5v3x.l7scAw.ZYaJ0SzwG', NULL, 'arjohn@admin.com', NULL, 'BhhPDqLczShZsuoTGgQv6.cc7a2757ba5a9fe53f', 1475979873, NULL, 1475978926, 1484631477, 1, 'Arjohn', 'Quijano', 'lucky88', '1234567890'),
 (3, '::1', 'heirocks', '$2y$08$fD.TQZMbjWiXn7IXdXGW5.K.w9fBPf7DgngS1qhY2/8XI9aWba8Ly', NULL, 'heiro.adriano@gmail.com', NULL, NULL, NULL, NULL, 1476098689, 1476188257, 1, 'Heiro', 'Adriano', 'admin', '09123456789');
 
 -- --------------------------------------------------------
@@ -500,6 +744,12 @@ ALTER TABLE `order_items`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `porder_items`
+--
+ALTER TABLE `porder_items`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
@@ -524,6 +774,12 @@ ALTER TABLE `product_unit`
   ADD PRIMARY KEY (`unit_id`);
 
 --
+-- Indexes for table `purchaseorder`
+--
+ALTER TABLE `purchaseorder`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `raw_materials`
 --
 ALTER TABLE `raw_materials`
@@ -536,9 +792,51 @@ ALTER TABLE `raw_materials_type`
   ADD PRIMARY KEY (`rmt_id`);
 
 --
+-- Indexes for table `receipts`
+--
+ALTER TABLE `receipts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `receipt_items`
+--
+ALTER TABLE `receipt_items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `receivingwh`
+--
+ALTER TABLE `receivingwh`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `receivingwh_items`
+--
+ALTER TABLE `receivingwh_items`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `salesorder`
 --
 ALTER TABLE `salesorder`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `spfreights`
+--
+ALTER TABLE `spfreights`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `splist`
+--
+ALTER TABLE `splist`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `spstat`
+--
+ALTER TABLE `spstat`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -564,7 +862,7 @@ ALTER TABLE `users_groups`
 -- AUTO_INCREMENT for table `contact`
 --
 ALTER TABLE `contact`
-  MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `cpfreights`
 --
@@ -574,7 +872,7 @@ ALTER TABLE `cpfreights`
 -- AUTO_INCREMENT for table `cplist`
 --
 ALTER TABLE `cplist`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT for table `cpstat`
 --
@@ -599,7 +897,12 @@ ALTER TABLE `login_attempts`
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `porder_items`
+--
+ALTER TABLE `porder_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `products`
 --
@@ -621,6 +924,11 @@ ALTER TABLE `product_ingredients`
 ALTER TABLE `product_unit`
   MODIFY `unit_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
+-- AUTO_INCREMENT for table `purchaseorder`
+--
+ALTER TABLE `purchaseorder`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT for table `raw_materials`
 --
 ALTER TABLE `raw_materials`
@@ -631,10 +939,45 @@ ALTER TABLE `raw_materials`
 ALTER TABLE `raw_materials_type`
   MODIFY `rmt_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
+-- AUTO_INCREMENT for table `receipts`
+--
+ALTER TABLE `receipts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `receipt_items`
+--
+ALTER TABLE `receipt_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `receivingwh`
+--
+ALTER TABLE `receivingwh`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `receivingwh_items`
+--
+ALTER TABLE `receivingwh_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `salesorder`
 --
 ALTER TABLE `salesorder`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `spfreights`
+--
+ALTER TABLE `spfreights`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `splist`
+--
+ALTER TABLE `splist`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `spstat`
+--
+ALTER TABLE `spstat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `users`
 --
